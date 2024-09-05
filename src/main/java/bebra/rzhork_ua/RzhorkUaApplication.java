3cbdf2d622f43cbc7b54802764c83a35b8d60915
Mykola Volokyta <nikv2005@gmail.com>
2024-09-05 12:12:06 +0300

    init commit

---
+++ src/main/java/bebra/rzhork_ua/RzhorkUaApplication.java
@@ -0,0 +1,14 @@
+package bebra.rzhork_ua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RzhorkUaApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Bebra");
+        SpringApplication.run(RzhorkUaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bebra/rzhork_ua/RzhorkUaApplicationTests.java
@@ -0,0 +1,13 @@
+package bebra.rzhork_ua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RzhorkUaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
